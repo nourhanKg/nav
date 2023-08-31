735f27047d25779aaa6480ba6a0d79b3e9198e79
Nourhan Aboelenen <Nourhan.Aboelenen@linkdev.com>
2023-08-31 17:46:48 +0300

    start

---
+++ src/root.component.js
@@ -0,0 +1,8 @@
+export default function Root(props) {
+  return <section>
+    <ul>
+      <li><a href="service1">Service1</a></li>
+      <li><a href="service2">Service2</a></li>
+    </ul>
+  </section>;
+}
